826c0f1d66c3346d41acbe4a27afff02087e812b
Animadversion <59497210+Animadversion@users.noreply.github.com>
2020-03-03 22:45:08 -0600

    now with 21 seconds faster!

--- commented.py
+++ commented.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
+# making easier to execute for linux users
 
 # importing numpy (a math program that does cool things)
 # numpy needs to be installed with pip3 (pip3 install numpy)
 import numpy as np
 # importing time from python
 import time 
-
-# first value in dictionary when it is created, using a number for easy management later
-trashable = 1
-# empty dictionary
-vadadoom = {}
-# tell program to get current time to calculate how long it takes
-start = time.time()
 
 # encrypted text to crack, numpy is used to turn these lists into a matrix
 EncodedMsg = np.array([
@@ -23,10 +17,18 @@ EncodedMsg = np.array([
     [15, -30, 24]
 ])
 
+# define an empty list to create matrix with
+matrix = []
+# create list for locations that will be avoided
+badlist = []
+
 # input test parameters when program started
 size = input("Input matrix size as a total number of values in matrix: ")
 depth = input("Range to test (positive integer): ")
 print('\n\n\n')
+
+# tell program to get current time to calculate how long it takes
+start = time.time()
 
 # ensure that inputs will create a square matrix
 try:
@@ -41,55 +43,31 @@ except:
     # if anything fails print message and exit program
     print('use square numbers such as 3x3 matrix = 9, 4x4 = 16')
     exit()
-# tell program how large the matrix is for later
-myspot = size
 
-# create the matrix with the minimum range for every value
-for i in range(1, singularsize + 1):
-    # create rows
-    for i in range(1, singularsize + 1):
-        # create the same number of columns by repeating instruction
-        # setting each key in dictionary to minimum range
-        vadadoom[trashable] = -depth
-        # add one to the first dictionary key to have a new key
-        trashable = trashable  + 1
-# replace the 2 known numbers in matrix
-vadadoom[1] = 1
-vadadoom[6] = 3
-# save original matrix to avoid duplicate output later and save time
-previousprint = str(vadadoom)
+# define x and y as largest values on the matrix grid
+x = singularsize
+y = x
+
+# create the matrix
+for i in range(0, size):
+    matrix.append(-depth)
+
+# change known values and add their locations to badlist
+matrix[0] = 1
+badlist.append(2)
+matrix[5] = 3
+badlist.append(137)
+
+# splice the matrix into square
+matrix = np.array_split(matrix, singularsize)
 
 # function to test keys and attempt to decode
-def makekey():
-    # declare the variable declared earlier in the program as global so it can be eddited freely
-    global previousprint
-    # create an empty, temporary list for each row of matrix
-    tmplist = []
-    # create an empty list for all rows of matrix
-    fulllist = []
-    # move to the first value in matrix
-    x = 1
-    # create the matrix
-    for i in vadadoom.values():
-        # for each key in the dictionary add that value to the temporary list
-        tmplist.append(i)
-        # move to the value in dictionary (once again why numbers used)
-        x = x + 1
-        # if the temporary list has the same amount of columns as the square matrix should have execute these instructions
-        if x  > singularsize:
-            # write the temporary list to the full list
-            fulllist.append(tmplist)
-            # set the temporary list to empty
-            tmplist = []
-            # move back to the first spot in temporary list
-            x = 1
-    # after the for loop is finished, use the full list to create a matrix using numpy
-    key = np.array(fulllist)
-    # have a switch to quickly skip executing if the inverse is a singular matrix
+def makekey():  
+# have a switch to quickly skip executing if the inverse is a singular matrix
     flip = 0
     # attempt to create an inverse matrix
     try:
-        inverse = np.linalg.inv(key)
+        inverse = np.linalg.inv(matrix)
     # if matrix is singular flip the switch
     except np.linalg.LinAlgError:
         flip = 1
@@ -118,14 +96,14 @@ def makekey():
                     flip = 1
                     break
     # if switch is flipped and key wasnt previously found, show the output!!!!
-    if flip == 0 and previousprint != str(vadadoom):
+    if flip == 0:
         # obtain the current time
         now = time.time()
         # calculate how long program took to execute
         print('Found in: ' + str(round((now - start), 3)) + ' seconds\n')
         # print encryption key
         print('key')
-        print(key)
+        print(np.array(matrix))
         # print inverse of encryption key
         print('\ninverse')
         print(inverse)
@@ -133,8 +111,6 @@ def makekey():
         print('\ndecoded')
         print(outputinverse)
         print('\n')
-        # save the current matrix to avoid duplicate output
-        previousprint = str(vadadoom)
         # reset the text conversion
         text = []
         # for row in decoded matrix
@@ -155,54 +131,47 @@ def makekey():
         # join the list of characters with nothing between and print
         print('Decoded Text: ' + ''.join(text).title())
         print('\n\n')
-        # write each decoded message to text file (can also be written to list or dictionary or anything really)
-        # i did not do this but this can be compared against wordlists to find the most likely correct solutions
-        # create a file called "matrixMessages.txt" and/or append to it
-        with open('matrixMessages.txt', 'a+') as f:
-            # join text with nothing between before writing
-            f.write(''.join(text) + '\r\n')
-            # close text file
-            f.close()
 
 
-# create a loop that runs until program is killed
-while True:
-    # execute program to test key as the first instruction of each loop
-    makekey()
-    # attempt to run program
-    try:
-        # if program reaches a known value, skip to next value
-        if myspot == 1 or myspot == 6:
-            myspot = myspot - 1
-        # if current value is greater than or equal to max value of range set it to the minimum value and move 1 spot forward in dictionary
-        # this is why the keys are numbers, to make it easy to traverse them no matter what size the dictionary is
-        elif vadadoom[myspot] >= depth:
-            vadadoom[myspot] = -depth
-            myspot = myspot -1
-        # if current is not in the first 2 values and is not greater than the max range, add one to that value and move back to the last value in dictionary
-        else:
-            vadadoom[myspot] = vadadoom[myspot] + 1
-            myspot = size
-    # the first key is 1, if the program reaches 0 it will exit
-    except KeyError:
-        now = time.time()
-        print('Program ran for: ' + str(round((now - start), 3)) + ' seconds\n')
-        exit()
+# traverse the matrix grid
+def movespot():
+    # declare variables global so entire program can use the changed values
+    global x, y
+    # if not in the final column, move 1 column left
+    if y > 1:
+        y = y - 1
+    # if final column reached
+    else:
+        # return to far right column
+        y = singularsize
+        # move up 1 row
+        x = x - 1
 
+# loop for while not at end of list
+while x > 0:
+    # define location using the sum of x to a prime and y square to save time
+    location = x ** 7 + y ** 2
+    # find value at current location beforehand to save time 
+    value = matrix[x - 1][y - 1]
+    # check if location can be changed, if not, skip it 
+    if location in badlist:
+        movespot()
+    # check if current value is greater than max value and if so, set to the min range then move to next location
+    elif value >= depth:
+        # check for possible key
+        makekey()
+        matrix[x - 1][y - 1] = -depth
+        movespot()
+    # if value is not greater than max and allowed to be changed
+    else:
+        # check for possible key
+        makekey()
+        # add 1 to value 
+        matrix[x - 1][y - 1] = value + 1
+        # move to the last value in list
+        x = singularsize
+        y = singularsize
 
-# correct outputs, program does not know these
-# key inverse
-# [[ -1. -10.  -8.]
-#  [ -1.  -6.  -5.]
-#  [ -0.  -1.  -1.]]
-# decrypted 
-# [[ 8.  1. 18.]
-#  [ 4.  0. 23.]
-#  [15. 18. 11.]
-#  [ 0. 16.  1.]
-#  [25. 19.  0.]
-#  [15.  6.  6.]]
-# key 
-# [1, -2, 2],
-# [-1, 1, 3],
-# [1, -1, -4]
+# print total time to run program
+now = time.time()
+print('Program ran for: ' + str(round((now - start), 3)) + ' seconds\n')
--- nocomments.py
+++ nocomments.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import time 
-
-trashable = 1
-vadadoom = {}
-start = time.time()
+import time
 
 EncodedMsg = np.array([
     [25, -33, -53],
@@ -16,11 +12,12 @@ EncodedMsg = np.array([
     [15, -30, 24]
 ])
 
+matrix = []
 
 size = input("Input matrix size as a total number of values in matrix: ")
 depth = input("Range to test (positive integer): ")
 print('\n\n\n')
-
+start = time.time()
 
 try:
     depth = int(depth)
@@ -31,34 +28,24 @@ try:
 except:
     print('use square numbers such as 3x3 matrix = 9, 4x4 = 16')
     exit()
-myspot = size
 
+badlist = []
+previoustext = str('a')
+x = singularsize
+y = x
+for i in range(0, size):
+    matrix.append(-depth)
+matrix[0] = 1
+badlist.append(2)
+matrix[5] = 3
+badlist.append(137)
+matrix = np.array_split(matrix, 3)
 
-for i in range(1, singularsize + 1):
-    for i in range(1, singularsize + 1):
-        vadadoom[trashable] = -depth
-        trashable = trashable  + 1
-vadadoom[1] = 1
-vadadoom[6] = 3
-previousprint = str(vadadoom)
-
-
-def makekey():
-    global previousprint  
-    tmplist = [] 
-    fulllist = []
-    x = 1
-    for i in vadadoom.values():
-        tmplist.append(i)
-        x = x + 1
-        if x  > singularsize:
-            fulllist.append(tmplist)
-            tmplist = []
-            x = 1
-    key = np.array(fulllist)
+def makekey():  
+    global previoustext
     flip = 0
     try:
-        inverse = np.linalg.inv(key)
+        inverse = np.linalg.inv(matrix)
     except np.linalg.LinAlgError:
         flip = 1
     if flip == 0: 
@@ -74,17 +61,7 @@ def makekey():
                 if item < 0 or item > 26:  
                     flip = 1
                     break
-    if flip == 0 and previousprint != str(vadadoom):
-        now = time.time()
-        print('Found in: ' + str(round((now - start), 3)) + ' seconds\n')
-        print('key')
-        print(key)
-        print('\ninverse')
-        print(inverse)
-        print('\ndecoded')
-        print(outputinverse)
-        print('\n')
-        previousprint = str(vadadoom)
+    if flip == 0:
         text = []
         for value in outputinverse:
             for item in value:
@@ -93,25 +70,42 @@ def makekey():
                     text.append(chr(160))
                 else:
                     text.append(chr(item + 96))
-        print('Decoded Text: ' + ''.join(text).title())
-        print('\n\n')
-        with open('matrixMessages.txt', 'a+') as f:
-            f.write(''.join(text) + '\r\n')
+        newtext = str(''.join(text).title())
+        if newtext != previoustext:
+            previoustext = newtext
+            now = time.time()
+            print('Found in: ' + str(round((now - start), 3)) + ' seconds')
+            print('Key')
+            print(np.array(matrix))
+            print('Inverse')
+            print(inverse)
+            print('Decoded Matrix')
+            print(outputinverse)
+            print('Decoded Text: ' + newtext)
+            print('\n')
 
 
+def movespot():
+    global x, y
+    if y > 1:
+        y = y - 1
+    else:
+        y = singularsize
+        x = x - 1
 
-while True:
-    makekey()
-    try:
-        if myspot == 1 or myspot == 6:
-            myspot = myspot - 1
-        elif vadadoom[myspot] >= depth:
-            vadadoom[myspot] = -depth
-            myspot = myspot -1
-        else:
-            vadadoom[myspot] = vadadoom[myspot] + 1
-            myspot = size
-    except KeyError:
-        now = time.time()
-        print('Program ran for: ' + str(round((now - start), 3)) + ' seconds\n')
-        exit()
+while x > 0:
+    location = x ** 7 + y ** 2
+    value = matrix[x - 1][y - 1]
+    if location in badlist:
+        movespot()
+    elif value >= depth:
+        matrix[x - 1][y - 1] = -depth
+        movespot()
+    else:
+        makekey()
+        matrix[x - 1][y - 1] = value + 1
+        x = singularsize
+        y = singularsize
+
+now = time.time()
+print('Program ran for: ' + str(round((now - start), 3)) + ' seconds\n')
